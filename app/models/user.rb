516f8fcf55141ef5fd26ddbb08eef94015dea9f7
antomo <tomoan0121@gmail.com>
2022-05-22 00:57:26 +0900

    tmp

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable,
-         :registerable
+  devise :database_authenticatable, #:registerable,
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 end

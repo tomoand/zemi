083910585d3885f3b06a6f11bc40b33c4a0752e8
antomo <tomoan0121@gmail.com>
2022-05-22 02:57:55 +0900

    mount

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  config.action_cable.mount_path = nil
+  config.action_cable.mount_path = '/websocket'
   config.action_cable.url = 'wss://hurikakegohan.herokuapp.com/websocket'
   config.action_cable.allowed_request_origins = [ 'https://tomoand.github.io' ]
 

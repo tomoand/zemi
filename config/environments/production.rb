797d8745bc42463a048c6d191fa2079316226a80
antomo <tomoan0121@gmail.com>
2022-05-22 02:52:26 +0900

    websocket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,9 +27,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.mount_path = nil
+  config.action_cable.url = 'wss://hurikakegohan.herokuapp.com/websocket'
+  config.action_cable.allowed_request_origins = [ 'https://tomoand.github.io' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
